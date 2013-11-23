0c7fa664c825709143d1b34b27c29c0df780a93c
Jake Tesler <jake.tesler@gmail.com>
2013-11-23 00:21:23 -0500

    removed additional terminal cmd

--- include/config_cmd_additions.h
+++ include/config_cmd_additions.h
@@ -20,22 +20,29 @@
  * These commands were added here by us to experiment with the UDOO U-Boot image. 
  */
 
-//#define CONFIG_CMD_DIAG		/* (POST?) Diagnostics			*/
+
 #define CONFIG_CMD_BSP		/* Board Specific functions	*/
+#define CONFIG_CMD_ITEST	/* Integer (and string) test	*/
+#define CONFIG_CMD_REGINFO	/* Register dump		*/
+#define CONFIG_CMD_SDRAM	/* SDRAM DIMM SPD info printout */
+#define CONFIG_CMD_SNTP		/* SNTP support		*/
+
+
+//#define CONFIG_CMD_DIAG		/* (POST?) Diagnostics			*/
+
+
 //#define CONFIG_CMD_DTT		/* Digital Therm and Thermostat */ ##crashed compiler,
 //##cmd_dtt.c: In function ‘do_dtt’:
 //##cmd_dtt.c:34:28: error: ‘CONFIG_DTT_SENSORS’ undeclared (first use in this function)
 
 
-#define CONFIG_CMD_ITEST	/* Integer (and string) test	*/
-#define CONFIG_CMD_REGINFO	/* Register dump		*/
-#define CONFIG_CMD_SDRAM	/* SDRAM DIMM SPD info printout */
-#define CONFIG_CMD_SNTP		/* SNTP support		*/
-#define CONFIG_CMD_TERMINAL	/* built-in Serial Terminal	*/
+//#define CONFIG_CMD_TERMINAL	/* built-in Serial Terminal	*/ ##crashed compiler, 
+//##common/libcommon.a(cmd_terminal.o): In function `do_terminal':
+//##/home/ubuntu/udoocode/U-Boot_UDOO/common/cmd_terminal.c:45: undefined reference to `serial_reinit_all'
+
 
 //#define CONFIG_CMD_USB		/* USB Support			*/ ##crashed compiler, 
 //##/home/ubuntu/udoocode/U-Boot_UDOO/include/usb.h:203:2: error: #error USB Lowlevel not defined
-
 
 
 #endif	/* _CONFIG_CMD_ADDITIONS_H */
